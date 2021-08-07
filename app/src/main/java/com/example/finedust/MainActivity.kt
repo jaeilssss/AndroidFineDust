e3c61531c5c474c9fc1c105fcccb7a7f0e47b854
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-08-07 23:34:02 +0900

    fusedLocationProviderClient 을 이용해서 현재 위치 latitude , longtitude 찍기

--- app/src/main/java/com/example/finedust/MainActivity.kt
+++ app/src/main/java/com/example/finedust/MainActivity.kt
@@ -1,11 +1,92 @@
 package com.example.finedust
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.example.finedust.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var cancellationTokenSource : CancellationTokenSource ? =null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initVariables()
+        requestLocationPermission()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        cancellationTokenSource?.cancel()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSIONS &&
+                    grantResults[0]==PackageManager.PERMISSION_GRANTED
+
+        if(!locationPermissionGranted){
+            finish()
+        }else{
+            // fetchData
+
+                cancellationTokenSource = CancellationTokenSource()
+
+
+            fusedLocationProviderClient.getCurrentLocation(
+                //high accuracy 는 정확도는 높지만 배터리를 많이 먹음
+                // 여기서는 어차피 한번만 요청 하므로 그냥 씀
+                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource!!.token
+
+                ).addOnSuccessListener { location ->
+                    binding.textview.text = "${location.latitude} , ${location.longitude}"
+            }
+
+        }
+    }
+
+    private fun initVariables(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun requestLocationPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSIONS
+        )
+    }
+
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSIONS = 100
     }
 }
